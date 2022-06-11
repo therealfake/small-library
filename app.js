637bdb1068336eb34370fa7ecab788b2c1045b17
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-06-10 21:23:17 -0400

    create general styling for each book 'card'

--- app.js
+++ app.js
@@ -31,20 +31,40 @@ function displayBooks() {
     for(let book of myLibrary) {
         card = document.createElement('div');
         card.classList.add('card')
-        
-        info = document.createElement('div');
-        info.textContent = book.info();
-        
-        removebtn = document.createElement('button');
-        removebtn.textContent = 'Remove'
 
-        card.appendChild(info);
-        card.appendChild(removebtn);
+        title = document.createElement('div');
+        title.textContent = book.title
+        title.classList.add('title');
+
+        author = document.createElement('div');
+        author.textContent = book.author
+        author.classList.add('author');
+        
+        pages = document.createElement('div');
+        pages.textContent = `${book.pages} pages`
+        pages.classList.add('pages');
+
+        book_status = document.createElement('button');
+        book_status.textContent = book.status
+        book_status.classList.add('status-btn');
+        if (book.status == "Read") { 
+            book_status.classList.add('read');
+        }
+        book_status.addEventListener('click', (e) => {
+            e.target.classList.toggle('read');
+            e.target.textContent === "Read" ? e.target.textContent = "Not Read Yet": e.target.textContent = "Read";
+        })
+
+        removebtn = document.createElement('button');
+        removebtn.classList.add('remove-btn');
+        removebtn.textContent = 'Remove';
+
+        card.append(title, author, pages, book_status, removebtn)
         bookshelf.appendChild(card);
     }
 }
 
-first = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+first = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Read');
 addBookToLibrary(first);
 displayBooks();
 
